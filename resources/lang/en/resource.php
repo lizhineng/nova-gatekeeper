53c55680792dd4a7d371f622d4cc5935870229b2
Li Zhineng <im@zhineng.li>
2021-08-08 15:14:19 +0800

    missing en lang

---
+++ resources/lang/en/resource.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'access_control' => 'Access Control',
+    'roles' => 'Roles',
+    'role' => 'Role',
+    'permissions' => 'Permissions',
+    'permission' => 'Permission',
+    'role_name' => 'Name',
+    'permission_name' => 'Name',
+    'creation_date' => 'Creation Date',
+    'last_update' => 'Last Update',
+];
